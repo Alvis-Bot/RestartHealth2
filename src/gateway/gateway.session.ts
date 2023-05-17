10c107230826d629fae7616f07cb0962e3d0e062
Võ Việt <vietvd@thomi.com.vn>
2023-05-17 09:57:00 +0700

    a

--- src/gateway/gateway.session.ts
+++ src/gateway/gateway.session.ts
@@ -1,41 +1,34 @@
 import { Injectable } from '@nestjs/common';
-import {AuthenticatedSocket} from "../common/inteface";
-
-export interface IGatewaySessionManager {
-    getUserSocket(id: number): string[]
-    setUserSocketId(id: number, socket: string): void
-    removeUserSocketId(id: number): void
-    getSockets(): Map<number, string[]>
-}
 
 @Injectable()
-export class GatewaySessionManager implements IGatewaySessionManager {
+export class GatewaySessionManager {
+  private readonly sessions: Map<number, string[]> = new Map<
+    number,
+    string[]
+  >();
+  getUserSocket(id: number) {
+    return this.sessions.get(id);
+  }
 
-     private readonly sessions : Map<number , string[]> = new Map<number, string[]>();
-    getUserSocket(id: number) {
-        return this.sessions.get(id);
+  setUserSocketId(id: number, socket: string) {
+    this.sessions.set(id, [...(this.sessions.get(id) || []), socket]);
+  }
+
+  removeUserSocketId(id: number) {
+    this.sessions.delete(id);
+  }
+
+  removeSocketId(id: number, socket: string) {
+    const sockets = this.sessions.get(id);
+    if (sockets) {
+      const index = sockets.indexOf(socket);
+      if (index > -1) {
+        sockets.splice(index, 1);
+      }
     }
+  }
 
-    setUserSocketId(id: number, socket: string) {
-        this.sessions.set(id, [...this.sessions.get(id) || [], socket]);
-    }
-
-    removeUserSocketId(id: number) {
-        this.sessions.delete(id);
-    }
-
-    removeSocketId(id: number, socket: string) {
-        const sockets = this.sessions.get(id);
-        if(sockets) {
-            const index = sockets.indexOf(socket);
-            if(index > -1) {
-                sockets.splice(index, 1);
-            }
-        }
-    }
-
-    getSockets() {
-        return this.sessions;
-    }
-
+  getSockets() {
+    return this.sessions;
+  }
 }
