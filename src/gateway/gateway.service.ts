a3735b2e5f377499e0fec8db92e27533499fd6ec
Võ Việt <vietvd@thomi.com.vn>
2023-05-17 20:03:33 +0700

    init

--- src/gateway/gateway.service.ts
+++ src/gateway/gateway.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from "@nestjs/websockets";
-import {Socket} from "socket.io";
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer
+} from "@nestjs/websockets";
+import { Server, Socket } from 'socket.io';
 import { GatewaySessionManager } from "./gateway.session";
 
 @Injectable()
@@ -12,12 +18,21 @@ import { GatewaySessionManager } from "./gateway.session";
 })
 export class GatewayService implements  OnGatewayConnection, OnGatewayDisconnect {
 
+
+  @WebSocketServer()
+  server: Server;
   constructor(private readonly sessions: GatewaySessionManager) {
   }
   handleConnection(client: Socket, ...args: any[]): any {
     console.log('client connected');
     console.log(client.id);
     this.sessions.setUserSocketId(1, client.id);
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(client: Socket, message: string) {
+    console.log('Received message from client: ' + message);
+    this.server.emit('message', 'Server received your message: ' + message);
   }
 
   handleDisconnect(client: Socket): any {
